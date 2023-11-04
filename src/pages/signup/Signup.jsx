ae8c3e104cfb4296ef41aa86fc4fc52e264be82b
skabeo <spencerokyere95@gmail.com>
2023-11-04 12:55:17 +0000

    Refactor error logic

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -12,7 +12,7 @@ const Signup = () => {
 
   const [showPassword, setShowPassword] = useState(false)
   const [showPasswordConfirmation, setShowPasswordConfirmation] = useState(false)
-  const [errorMessage, setErrorMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
@@ -30,122 +30,138 @@ const Signup = () => {
   }
 
   const errorMessages = () => {
-    const specialChar = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/;
+    const errors = []
+    const specialChar = /[!@#$%^&*()_+{}[\]:;<>,.?~-]/;
 
     if (formData.password !== formData.passwordConfirmation) {
-      setErrorMessage('Password do not match')
-    } else if (!specialChar.test(formData.password)) {
-      setErrorMessage('Password must contain at least one special character')
-    } else if (formData.email === formData.password) {
-      setErrorMessage('Password should not be the same as email')
-    } else if (formData.password.length < 6 ) {
-      setErrorMessage('Password should have at least 6 characters')
+      errors.push('Passwords do not match');
     }
+  
+    if (!specialChar.test(formData.password)) {
+      errors.push('Password must contain at least one special character');
+    }
+  
+    if (formData.email === formData.password) {
+      errors.push('Password should not be the same as the email');
+    }
+  
+    if (formData.password.length < 6) {
+      errors.push('Password should have at least 6 characters');
+    }
+
+    return errors;
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    errorMessages()
+    const errors = errorMessages()
+    setErrorMessage(errors)
   }
   
 
   return (
-    <section>
+    <section >
       <SessionHeader />
-      <form onSubmit={handleSubmit} className='signup-form'>
+      <div className='signup-section-container'>
         <span className='signup-back-button-container'>
           <img className='signup-back-button' src={back} alt='back-button' />
         </span>
-        <p className='font-bold text-xl'>Registration</p>
-        {errorMessage && <p className='signup-error-message'>{errorMessage}</p>}
-        <div className='signup-firstname-container'>
-          <label htmlFor='firstName'></label>
-          <input 
-            id='firstName'
-            type='text'
-            name='firstName'
-            placeholder='First name'
-            value={formData.firstName}
-            onChange={handleInputChange}
-            required 
-          />
-        </div>
+        <p className='font-bold text-xl my-4'>Registration</p>
+        <ul className='list-disc pl-4'>
+          {errorMessage ? errorMessage.map((error, index) => (
+            <li key={index} className='signup-error-message'>{error}</li>
+          )) : null}
+        </ul>
+        <form onSubmit={handleSubmit} className='signup-form'>
+          <div className='signup-firstname-container mt-3'>
+            <label htmlFor='firstName'></label>
+            <input 
+              id='firstName'
+              type='text'
+              name='firstName'
+              placeholder='First name'
+              value={formData.firstName}
+              onChange={handleInputChange}
+              required 
+            />
+          </div>
 
-        <div>
-          <label htmlFor='lastName'></label>
-          <input 
-            id='lastName'
-            type='text'
-            name='lastName'
-            placeholder='Last name'
-            value={formData.lastName}
-            onChange={handleInputChange}
-            required 
-          />
-        </div>
+          <div>
+            <label htmlFor='lastName'></label>
+            <input 
+              id='lastName'
+              type='text'
+              name='lastName'
+              placeholder='Last name'
+              value={formData.lastName}
+              onChange={handleInputChange}
+              required 
+            />
+          </div>
 
-        <div>
-          <label htmlFor='email'></label>
-          <input 
-            id='email'
-            type='text'
-            name='email'
-            placeholder='Email'
-            value={formData.email}
-            onChange={handleInputChange}
-            required 
-          />
-        </div>
+          <div>
+            <label htmlFor='email'></label>
+            <input 
+              id='email'
+              type='text'
+              name='email'
+              placeholder='Email'
+              value={formData.email}
+              onChange={handleInputChange}
+              required 
+            />
+          </div>
 
-        <div className='signup-password-container'>
-          <label htmlFor='password'></label>
-          <input 
-            id='password'
-            type={ showPassword ? 'text' : 'password' }
-            name='password'
-            placeholder='Password'
-            value={formData.password}
-            onChange={handleInputChange}
-            required 
-          />
-          <span
-            onClick={() => setShowPassword(!showPassword)}
-            className='signup-password-icon'
-          >
-            {showPassword ? <FaEye /> : <FaEyeSlash />}
-          </span> 
-        </div>
+          <div className='signup-password-container'>
+            <label htmlFor='password'></label>
+            <input 
+              id='password'
+              type={ showPassword ? 'text' : 'password' }
+              name='password'
+              placeholder='Password'
+              value={formData.password}
+              onChange={handleInputChange}
+              required 
+            />
+            <span
+              onClick={() => setShowPassword(!showPassword)}
+              className='signup-password-icon'
+            >
+              {showPassword ? <FaEye /> : <FaEyeSlash />}
+            </span> 
+          </div>
 
-        <div className='signup-password-container'>
-          <label htmlFor='passwordConfirmation'></label>
-          <input 
-            id='passwordConfirmation'
-            type={showPasswordConfirmation ? 'text' : 'password'}
-            name='passwordConfirmation'
-            placeholder='Confirm password'
-            value={formData.passwordConfirmation}
-            onChange={handleInputChange}
-            required 
-          />
-          <span
-            onClick={() => setShowPasswordConfirmation(!showPasswordConfirmation)}
-            className='signup-password-icon'
-          >
-            {showPasswordConfirmation ? <FaEye /> : <FaEyeSlash />}
-          </span> 
-        </div>
+          <div className='signup-password-container'>
+            <label htmlFor='passwordConfirmation'></label>
+            <input 
+              id='passwordConfirmation'
+              type={showPasswordConfirmation ? 'text' : 'password'}
+              name='passwordConfirmation'
+              placeholder='Confirm password'
+              value={formData.passwordConfirmation}
+              onChange={handleInputChange}
+              required 
+            />
+            <span
+              onClick={() => setShowPasswordConfirmation(!showPasswordConfirmation)}
+              className='signup-password-icon'
+            >
+              {showPasswordConfirmation ? <FaEye /> : <FaEyeSlash />}
+            </span> 
+          </div>
 
-        <div className='password-guide'>
-          <h5 className='text-xs font-bold mb-1'>Your password must:</h5>
-          <ul className="list-disc pl-4">
-            <li className='text-xs'>Have between 6 and 20 charcters</li>
-            <li className='text-xs'>Contain at least 1 special charcter</li>
-            <li className='text-xs'>Be different from your email address</li>
-          </ul>
-        </div>
+          <div className='password-guide'>
+            <h5 className='text-xs font-bold mb-1'>Your password must:</h5>
+            <ul className="list-disc pl-4">
+              <li className='text-xs'>Have between 6 and 20 charcters</li>
+              <li className='text-xs'>Contain at least 1 special charcter</li>
+              <li className='text-xs'>Be different from your email address</li>
+            </ul>
+          </div>
 
-        <button className='signup-submit-button' type='submit'>Sign Up</button>
-      </form>
+          <button className='signup-submit-button' type='submit'>Sign Up</button>
+        </form>
+      </div>
     </section>
   )
 }
