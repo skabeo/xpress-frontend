180e91739f55334b8fa55c74405c15092c538d25
skabeo <spencerokyere95@gmail.com>
2023-12-17 22:03:11 +0000

    Dispatch a function to fetch all products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import { useEffect } from "react";
 import Sidebar from "../components/sidebar/Sidebar";
+import { useDispatch } from "react-redux";
+import { fetchProducts } from "../redux/products/productSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+
   useEffect(() => {
     document.title = 'Home | Xpress';
+  }, []);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
   }, []);
   
   return (
