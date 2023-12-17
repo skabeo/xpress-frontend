79dfa3f297d43c56364f60fc1b13d6776fc793cb
skabeo <spencerokyere95@gmail.com>
2023-12-17 22:03:51 +0000

    Add another slice to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sessionReducer from "./session/sessionSlice";
+import productReducer from "./products/productSlice";
 
 const store = configureStore({
   reducer: {
     session: sessionReducer,
-  }
+    product: productReducer
+  },
 });
 
 export default store;
