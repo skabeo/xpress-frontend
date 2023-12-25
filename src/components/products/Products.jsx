a60d15ce89ddc7d9998734e7637ee58deeefc2c6
skabeo <spencerokyere95@gmail.com>
2023-12-25 09:36:31 +0000

    Create a card

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './styles/product.css'
+import house from '../../assets/house.jpg'
+
+const Products = () => {
+  return (
+    <div>
+      <h1>Products</h1>
+      <div className='products-home-grid'>
+        <div className='product-image-container'>
+          <img className='product-image' src={house} alt='house' />
+          <span className='product-price'>
+            <span className='text-sm'>at</span> 
+            <span className='font-bold'> ₵200</span>
+          </span>
+        </div>
+        <div className='product-desc-display'>
+          <div>
+            <p className='text-sm'>120g</p>
+            <p className='font-bold text-sm'>Ladies cup</p>
+          </div>
+          <div>
+            <button className='products-view-deal'>View deal</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Products
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
+import './styles/home.css'
 import Sidebar from "../components/sidebar/Sidebar";
 import { useDispatch } from "react-redux";
 import { fetchProducts } from "../redux/products/productSlice";
+import Products from "../components/products/Products";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -17,6 +19,9 @@ const Home = () => {
   return (
     <div>
       <Sidebar />
+      <div className="products-grid">
+        <Products />
+      </div>
     </div>  
   )
 }
