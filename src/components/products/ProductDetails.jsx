59ffd3d88b1f87c247a804d81ef5935e33eea078
skabeo <spencerokyere95@gmail.com>
2023-12-25 21:25:29 +0000

    Retrieve specific product

--- src/components/products/ProductDetails.jsx
+++ src/components/products/ProductDetails.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Sidebar from '../sidebar/Sidebar';
 import './styles/product-details.css'
 import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProducts } from '../../redux/products/productSlice';
 
 const ProductDetails = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.product.products);
   const { id } = useParams();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  const specificProduct = Array.isArray(products) ? products.find(product => product.id === parseInt(id)) : null;
+  console.log(specificProduct)
+
   return (
     <div className='product-details-container'>
       <Sidebar />
