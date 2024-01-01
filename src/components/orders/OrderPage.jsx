5b3fc63e7b807634186cb1a5f8013141a61da2f0
skabeo <spencerokyere95@gmail.com>
2024-01-01 19:29:25 +0000

    Add order page heading

--- src/components/orders/OrderPage.jsx
+++ src/components/orders/OrderPage.jsx
@@ -1,6 +1,21 @@
+import Sidebar from "../sidebar/Sidebar"
+import './styles/order-page.css'
+
 const OrderPage = () => {
   return (
-    <div>Order here</div>
+    <div className="order-page-container">
+      <Sidebar />
+      <div>
+        <div className="order-page-heading">
+          <p>Checkout (1 item)</p>
+        </div>
+        <hr />
+        <div className="order-page-section">
+          <div>Hey</div>
+          <div>Hello</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
