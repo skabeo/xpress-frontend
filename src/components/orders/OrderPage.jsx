1f646028a3278975f9f25b66086049d56a7cb039
skabeo <spencerokyere95@gmail.com>
2024-01-03 22:18:00 +0000

    Improve order page

--- src/components/orders/OrderPage.jsx
+++ src/components/orders/OrderPage.jsx
@@ -1,5 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Sidebar from "../sidebar/Sidebar"
 import './styles/order-page.css'
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
 
 const OrderPage = () => {
   return (
@@ -11,8 +13,36 @@ const OrderPage = () => {
         </div>
         <hr />
         <div className="order-page-section">
-          <div className="order-first-section">Hey</div>
-          <div>Hello</div>
+          <div className="order-first-section">
+            <div>
+              <p>Shipping address</p>
+              <div className="inner-order-list">
+                <p className="font-bold text-sm">Your address</p>
+                <hr />
+                <div className="radio-address">
+                  <input 
+                    type="radio"
+                    name="address"
+                    checked
+                  />
+                  <span className="text-xs">Spencer Okyere B1023/24 Obonu St, Accra, Ghana.<span>Edit address</span></span>
+                </div>
+                <div>
+                  <span></span>
+
+                  <FontAwesomeIcon icon={faPlus} style={{ color: 'gray' }}/>
+                  <span>Add Address</span>
+                </div>
+              </div>
+            </div>
+            <div>
+              <p>Items and shipping</p>
+            </div>
+            <div>
+              <p>Payment method</p>
+            </div>
+          </div>
+          <div className="order-sec-section">Hello</div>
         </div>
       </div>
     </div>
